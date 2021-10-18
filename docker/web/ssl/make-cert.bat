9558d147139eb2e4ec166113630411d8dd490818
Oskar <oskarzborowski@gmail.com>
2021-10-18 19:08:40 +0200

    initial commit

--- docker/web/ssl/make-cert.bat
+++
@@ -1,6 +0,0 @@
-openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout ./cert/RootCA.key -out ./cert/RootCA.pem -subj "/C=US/CN=Example-Root-CA" &
-openssl x509 -outform pem -in ./cert/RootCA.pem -out ./cert/RootCA.crt &
-openssl req -new -nodes -newkey rsa:2048 -keyout ./cert/localhost.key -out ./cert/localhost.csr -subj "/C=PL/L=Warsaw/O=Example-Certificates/CN=localhost.local" &
-openssl x509 -req -sha256 -days 1024 -in ./cert/localhost.csr -CA ./cert/RootCA.pem -CAkey ./cert/RootCA.key -CAcreateserial -extfile domains.ext -out ./cert/localhost.crt
-
-pause
--- docker/web/ssl/readme.md
+++
@@ -1,1 +0,0 @@
-https://gist.github.com/cecilemuller/9492b848eb8fe46d462abeb26656c4f8
