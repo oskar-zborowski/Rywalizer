6511bb146bf3fe36c72d56d17bed5b065e4479d4
kborowicz <krypi23@gmail.com>
2021-12-02 15:07:42 +0100

    Widok usera

--- resources/assets/components/form/Button/Button.tsx
+++ resources/assets/components/form/Button/Button.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styles from './Button.scss';
 
+export interface IButtonProps {
+    backgroundColor?: string;
+    color?: string;
+}
+
 export const OrangeButton: React.FC = (props) => {
     return (
         <button type="button" className={styles.orangeButton}>
@@ -12,6 +17,14 @@ export const OrangeButton: React.FC = (props) => {
 export const GrayButton: React.FC = (props) => {
     return (
         <button type="button" className={styles.grayButton}>
+            {props.children}
+        </button>
+    );
+};
+
+export const BlackButton: React.FC = (props) => {
+    return (
+        <button type="button" className={styles.blackButton}>
             {props.children}
         </button>
     );
