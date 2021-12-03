9e7d9698f91f21721bc61aa79a57c13357fec4e3
Oskar <oskarzborowski@gmail.com>
2021-12-03 20:58:05 +0100

    Naprawienie GitHubControllera v2

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\GitHubController;
+use App\Http\Controllers\Api\GitHubController;
 use Illuminate\Support\Facades\Route;
 
 /*
