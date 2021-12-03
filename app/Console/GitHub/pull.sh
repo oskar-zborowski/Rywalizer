4dda7eb3819ff06666c6357468ef38cf59b00269
Oskar <oskarzborowski@gmail.com>
2021-12-03 21:21:41 +0100

    Naprawienie GitHubControllera v5

--- app/Console/GitHub/pull.sh
+++ app/Console/GitHub/pull.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo git pull origin master
+# sudo git pull origin master
+
+cd ../../../ 
+echo "Fetching"
+git fetch
+echo "Pulling"
+git pull origin master
+echo "Done at `date`"
