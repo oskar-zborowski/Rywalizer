f8e0746f2fe8277c79a5186ae9334c6faf0162e1
Oskar <oskarzborowski@gmail.com>
2022-01-04 23:39:04 +0100

    Kontynuacja tworzenia modeli

---
+++ app/Models/MinimumSkillLevel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+class MinimumSkillLevel extends BaseModel
+{
+    protected $fillable = [
+        'sport_id',
+        'name',
+        'description',
+    ];
+
+    protected $guarded = [
+        'id',
+        'creator_id',
+        'editor_id',
+        'supervisor_id',
+        'visible_at',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $hidden = [
+        'id',
+        'sport_id',
+        'name',
+        'description',
+        'creator_id',
+        'editor_id',
+        'supervisor_id',
+        'visible_at',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'visible_at' => 'string',
+        'created_at' => 'string',
+        'updated_at' => 'string'
+    ];
+
+    public function sport() {
+        return $this->belongsTo(DefaultType::class, 'sport_id');
+    }
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function editor() {
+        return $this->belongsTo(User::class, 'editor_id');
+    }
+
+    public function supervisor() {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    public function announcements() {
+        return $this->hasMany(Announcement::class);
+    }
+}
---
+++ app/Models/SportsPosition.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+class SportsPosition extends BaseModel
+{
+    protected $fillable = [
+        'sport_id',
+        'name'
+    ];
+
+    protected $guarded = [
+        'id',
+        'creator_id',
+        'editor_id',
+        'supervisor_id',
+        'visible_at',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $hidden = [
+        'id',
+        'sport_id',
+        'name',
+        'creator_id',
+        'editor_id',
+        'supervisor_id',
+        'visible_at',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'visible_at' => 'string',
+        'created_at' => 'string',
+        'updated_at' => 'string'
+    ];
+
+    public function sport() {
+        return $this->belongsTo(DefaultType::class, 'sport_id');
+    }
+
+    public function creator() {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function editor() {
+        return $this->belongsTo(User::class, 'editor_id');
+    }
+
+    public function supervisor() {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    public function announcementsSeats() {
+        return $this->hasMany(AnnouncementSeat::class);
+    }
+}
